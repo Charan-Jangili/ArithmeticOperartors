52cb40dfd6b770357bf56a7ce679a48dbb623e54
Charan-Jangili <charanjangili01@gmail.com>
2025-01-25 12:19:25 +0530

    Add files via upload

---
+++ ArithmeticOprator.java
@@ -0,0 +1,11 @@
+class ArithmeticOperator { 
+static int a=10;
+static int b=20;
+    public static void main(String[] args) {
+        System.out.println("Additon of a and b is ="+(a+b));
+        System.out.println("Substraction of a and b is ="+(a-b));
+        System.out.println("Multiplication of a and b is ="+(a*b));
+        System.out.println("Divison of a and b is ="+(a/b));
+
+    }
+}
